f77c556aedd9e9e8432754abceeb370f7de41632
Elìas <eignaciobazan.25cf@iesesteveterradas.cat>
2026-05-07 19:47:11 +0200

    DecoracionWEB.-Programadors

--- M04/server/app.js
+++ M04/server/app.js
@@ -15,9 +15,9 @@ const db = new MySQL();
 if (!isProxmox) {
   db.init({
     host: '127.0.0.1',
-    port: 3306,
-    user: 'root',
-    password: 'tuclave',
+    port: 3307,
+    user: 'super',
+    password: '1234',
     database: 'ProyectoMixII'
   });
 } else {
