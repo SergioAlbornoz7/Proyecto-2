a0385f4e1e3bbd63910b20bbf7014392035b762b
IvanLahoz <ilahozzafra.25cf@iesesteveterradas.cat>
2026-05-11 17:57:12 +0200

    Classe Battle creada

---
+++ M03/src/pkgmain/Battle.java
@@ -0,0 +1,19 @@
+package pkgmain;
+
+import java.util.ArrayList;
+
+public class Battle {
+	ArrayList<MilitaryUnit> civilizationArmy;
+	ArrayList<MilitaryUnit> enemyArmy;
+	ArrayList armies; //Ni idea de como poner esto
+	String battleDevelopment;
+	int initialCostFleet;
+	int initialNumberUnitsCivilization, initialNumbersUnitsEnemy;
+	int wasteWoodIron;
+	int enemyDrops;
+	int CivilizationDrops;
+	int resourcesLooses;
+	int initialArmies;
+	int actualNumberUnitsCivulization;
+	int actualNumberUnitsEnemy;
+}
